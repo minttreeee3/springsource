1418071a9faf5b938491c19e461686afc1ab76b2
minttreee3 <mintppp58@gmail.com>
2023-06-03 22:51:30 +0900

    security

--- security/src/main/java/com/spring/controller/SecurityController.java
+++ security/src/main/java/com/spring/controller/SecurityController.java
@@ -1,9 +1,13 @@
 package com.spring.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,19 +20,38 @@ public class SecurityController {
 	public void loginGet() {
 		log.info("로그인 폼 요청");		
 	}
+	
+	@PreAuthorize("hasAnyAuthority('ROLE_USER')")  //security-context.xml의 intercept-url과 같은 역할
 	@GetMapping("/userpage")
 	public void userPage() {
 		log.info("유저 페이지 요청");		
 	}
+	@PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
 	@GetMapping("/adminpage")
 	public void adminPage() {
 		log.info("관리자 페이지 요청");		
 	}
+	
+	
 	@GetMapping("/login-error")
 	public String loginError(Model model) {
 		model.addAttribute("error","아이디나 비밀번호를 확인해주세요");	
 		return "/security/login";
 	}
+	
+	@GetMapping("/access-denied")
+	public String accessDenied() {
+		return "/security/denied";		
+	}
+	
+	
+	@GetMapping("/auth")
+	@ResponseBody
+	public Authentication auth() {
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+	
+	
 	
 
 }
